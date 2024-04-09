7ad22029e52abf99c1a1bc6d6b7703868aa09843
KrHanwoo <71239208+KrHanwoo@users.noreply.github.com>
2024-04-09 23:34:20 +0900

    Update and rename firebase_config.template.js to firebase_config.js

---
+++ src/resources/js/data_channel/firebase_config.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+export const firebaseConfig = {
+  apiKey: 'AIzaSyDOMyWaOG8fMB5yQ0UYell45CtO_s-vBmc',
+  authDomain: 'pikavolley-online.firebaseapp.com',
+  databaseURL: 'https://pikavolley-online.firebaseio.com',
+  projectId: 'pikavolley-online',
+  storageBucket: 'pikavolley-online.appspot.com',
+  messagingSenderId: '16042256604',
+  appId: '1:16042256604:web:2caf500ea81c36f550ec19',
+  measurementId: 'G-ZCQ5ESKJQL',
+};
